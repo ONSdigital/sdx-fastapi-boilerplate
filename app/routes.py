3fe6232e9b62089c30abbd6ff472169e50fa3d66
Angus <angus.goody@ons.gov.uk>
2025-03-17 13:54:59 +0000

    format

--- app/routes.py
+++ app/routes.py
@@ -1,15 +1,13 @@
-
 from fastapi import APIRouter
-
 
 router = APIRouter()
 
 
 @router.get("/")
 async def root():
-	return {"message": "Welcome to the sdx-fastapi-boilerplate"}
+    return {"message": "Welcome to the sdx-fastapi-boilerplate"}
 
 
 @router.get("/health")
 async def health():
-	return {"message": "OK"}
+    return {"message": "OK"}
