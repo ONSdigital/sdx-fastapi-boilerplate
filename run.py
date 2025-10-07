6689c051b03fb69088bc2314cf9126311af98756
Angus <Angus.goody@ons.gov.uk>
2025-10-07 13:14:33 +0100

    updated project

--- run.py
+++ run.py
@@ -1,4 +1,12 @@
-from app.main import run
+from pathlib import Path
 
-if __name__ == "__main__":
-    run()
+from app.routes import router
+from app.settings import Settings
+from sdx_base.run import run
+
+async def dummy_tx_id_getter(request):
+    return "NA"
+
+if __name__ == '__main__':
+    proj_root = Path(__file__).parent
+    run(Settings, routers=[router], proj_root=proj_root, get_tx_id=dummy_tx_id_getter)
