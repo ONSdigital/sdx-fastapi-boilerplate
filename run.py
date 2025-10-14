d8adb224c98d7793392d3e39db5914e365d9aaa9
Angus <angus.goody@gmail.com>
2025-10-15 00:27:46 +0100

    updates

--- run.py
+++ run.py
@@ -1,12 +1,20 @@
 from pathlib import Path
 
+from sdx_base.server.server import RouterConfig
+
 from app.routes import router
 from app.settings import Settings
 from sdx_base.run import run
+
 
 async def dummy_tx_id_getter(request):
     return "NA"
 
 if __name__ == '__main__':
     proj_root = Path(__file__).parent
-    run(Settings, routers=[router], proj_root=proj_root, get_tx_id=dummy_tx_id_getter)
+
+    routers = [
+        RouterConfig(router=router, prefix="", tx_id_getter=dummy_tx_id_getter)
+    ]
+    
+    run(Settings, proj_root=proj_root, routers=routers)
