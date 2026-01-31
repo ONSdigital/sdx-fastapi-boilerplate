f6c925c98decc135063aef0cfe4f645bd4f0a4de
Angus <Angus.goody@ons.gov.uk>
2026-01-31 17:06:14 +0000

    using sdx-base default for tx_id getter

--- run.py
+++ run.py
@@ -1,19 +1,16 @@
 from pathlib import Path
 
 from sdx_base.server.server import RouterConfig
+from sdx_base.server.tx_id import txid_not_applicable
 
 from app.routes import router
 from app.settings import Settings
 from sdx_base.run import run
 
 
-async def dummy_tx_id_getter(request):
-    return "NA"
-
-
 if __name__ == "__main__":
     proj_root = Path(__file__).parent
 
-    routers = [RouterConfig(router=router, prefix="", tx_id_getter=dummy_tx_id_getter)]
+    routers = [RouterConfig(router=router, tx_id_getter=txid_not_applicable)]
 
     run(Settings, proj_root=proj_root, routers=routers)
