7a468db64ab6cc66604fd5724a9cbb85baae4cf4
Angus <Angus.goody@ons.gov.uk>
2026-05-12 13:58:11 +0100

    version

--- run.py
+++ run.py
@@ -1,16 +1,14 @@
-from pathlib import Path
 
 from sdx_base.server.server import RouterConfig
 from sdx_base.server.tx_id import txid_not_applicable
 
 from app.routes import router
-from app.settings import Settings
+from app.settings import Settings, ROOT
 from sdx_base.run import run
 
 
 if __name__ == "__main__":
-    proj_root = Path(__file__).parent
 
     routers = [RouterConfig(router=router, tx_id_getter=txid_not_applicable)]
 
-    run(Settings, proj_root=proj_root, routers=routers)
+    run(Settings, proj_root=ROOT, routers=routers)
